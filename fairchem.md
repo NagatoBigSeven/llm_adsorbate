0cf98558201c717f72fa5877634a4aa3203fc376
NagatoBigSeven <zzmhkust@gmail.com>
2025-12-29 22:00:05 +0100

    chore: remove unused files (fairchem.md, launch.sh)

--- fairchem.md
+++
@@ -1,7 +0,0 @@
-```zsh
-conda create -n fairchem python=3.11
-
-conda activate fairchem
-
-pip install numpy ase fairchem-core fairchem-demo-ocpapi
-```
--- launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-source /opt/anaconda3/etc/profile.d/conda.sh
-conda activate llm_adsorbate
-python -m src.agent.agent \
-  --smiles 'ClC(=O)[O-]' \
-  --slab "./notebooks/cu_slab_211.xyz" \
-  --user_request "does this fragment stay covalently bound through the carbon to a top site in the middle of the surface?"
